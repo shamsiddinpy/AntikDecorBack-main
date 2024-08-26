bd8192985efcaabfd0075ad72ec7e4a4dd80a57d
shamsiddindaminov <shamsiddinpython215@gmail.com>
2024-08-27 01:06:59 +0500

    .

--- apps/product/views.py
+++ apps/product/views.py
@@ -3,7 +3,6 @@ from django.shortcuts import get_object_or_404
 from drf_yasg import openapi
 from drf_yasg.utils import swagger_auto_schema
 from rest_framework import status
-from rest_framework.pagination import PageNumberPagination
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.views import APIView
